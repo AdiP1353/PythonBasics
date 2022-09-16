5ae6bce1edf3a3762dbcfc02e22467389ca166f1
Adi P <aprakash1353@gmail.com>
2022-09-16 19:26:12 +0100

    Started text game

--- regex.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-span_tags = ["<span>97</span>", "<span>97</span>", "<span>90</span>", "<span>90</span>", "<span>88</span>", "<span>87</span>", "<span>87</span>", "<span>80</span>", "<span>79</span>", "<span>79</span>", "<span>78</span>", "<span>76</span>", "<span>76</span>", "<span>72</span>", "<span>72</span>", "<span>66</span>", "<span>66</span>", "<span>65</span>", "<span>65</span>", "<span>64</span>", "<span>61</span>", "<span>61</span>", "<span>59</span>", "<span>58</span>", "<span>57</span>", "<span>57</span>", "<span>54</span>", "<span>51</span>", "<span>49</span>", "<span>47</span>", "<span>40</span>", "<span>38</span>", "<span>37</span>", "<span>36</span>", "<span>36</span>", "<span>32</span>", "<span>25</span>", "<span>24</span>", "<span>22</span>", "<span>21</span>", "<span>19</span>", "<span>18</span>", "<span>18</span>", "<span>14</span>", "<span>12</span>", "<span>12</span>", "<span>9</span>," "<span>7</span>," "<span>3</span>," "<span>2</span>]"
-
-for tag in span_tags:
-    print(tag)
