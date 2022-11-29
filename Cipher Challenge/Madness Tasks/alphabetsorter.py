3e54f05616b8a13c081ced96a9f6f16d4529062e
Adi P <aprakash1353@gmail.com>
2022-11-29 20:05:57 +0000

    29 Nov 2022, 20:05

--- Cipher Challenge/Madness Tasks/alphabetsorter.py
+++ Cipher Challenge/Madness Tasks/alphabetsorter.py
@@ -3,13 +3,17 @@ fhand = open("finalcorpus.txt","r").read()
 wordl = fhand.split(" ")
 wordl.sort()
 
+print("Word list sorted")
+
 wordl = list(dict.fromkeys(wordl))
-print(wordl)
+print("Duplicates removed")
 
-# fhand2 = open("alphabetcorpus.txt","a")
-
-# for thing in wordl:
-#     fhand3 = open("alphabetcorpus.txt","r").read()
-#     if thing not in fhand3:
-#         fhand2.write(thing + '\n')
-    
+fhand2 = open("alphabetcorpus.txt","a")
+i = 0
+for thing in wordl:
+    fhand3 = open("alphabetcorpus.txt","r").read()
+    if thing not in fhand3:
+        fhand2.write(thing + '\n')
+        print("it" + str(i))
+        i += 1
+print("List written")
