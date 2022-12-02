67066a4732be6f0e2cb0253291df7299de3f6014
Adi P <aprakash1353@gmail.com>
2022-12-02 18:41:07 +0000

    Frequency sorter working, pretty slow tho

--- Cipher Challenge/Madness Tasks/frequencysorter.py
+++ Cipher Challenge/Madness Tasks/frequencysorter.py
@@ -1,18 +1,42 @@
-fhand = open("finalcorpus.txt","r").read()
+def remove_items(test_list, item):
+ 
+    # remove the item for all its occurrences
+    c = test_list.count(item)
+    for i in range(c):
+        test_list.remove(item)
+ 
+    return test_list
 
-wordl = fhand.split(" ")
-wordl.sort()
+def frequency_calculator(filein: str, fileout: str):
+    fhand = open(filein,"r").read()
 
-frequency_output = open("frequency_output.txt",'a')
-print("Word list sorted")
-word_no_dupes = list(dict.fromkeys(wordl))
-word_count = len(word_no_dupes)
-print(f"{word_count} Unique words detected")
-frequencies = []
-for i, thing in enumerate(word_no_dupes):
-    count = wordl.count(thing)
-    #frequency_output.write(f"{thing}:{count},\n")
-    frequencies.append(f"{thing}: {count},")
-    print("In progress:" + str(i/word_count*100) + "%")
+    wordl = fhand.split(" ")
+    wordl.sort()
 
-frequency_output.writelines(frequencies)
+    print("Word list sorted")
+
+    frequency_output = open(fileout,'a')
+
+
+    frequency_list = []
+
+    i = 0
+    for thing in wordl:
+        count = wordl.count(thing)
+        frequency_list.append(f"{thing}: {count}\n")
+        remove_items(wordl, thing)
+        print(f"Iteration {i}")
+        i += 1
+
+    print("Frequency List Created")
+
+    for thing in frequency_list:
+        frequency_output.write(thing)
+
+    print("Frequency List written")
+
+
+
+
+
+
